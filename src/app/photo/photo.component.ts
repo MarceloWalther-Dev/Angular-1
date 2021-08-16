484e9c7b3db5e298a25fdaf50e706e4d76a20938
MarceloWalther-Dev <marcelo.walther.10@gmail.com>
2021-08-16 14:12:20 -0300

    Inbound properties: passando dados para componente

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: 'app-photo',
+  templateUrl: './photo.component.html',
+  //styleUrls: []
+})
+export class PhotoComponent{
+
+  @Input() description = ''; // @Input vem do app.component.html, o atributo description é preenchido com o valor que vem do app-component.html
+  @Input() url = ''; // @Input vem do app.component.html, o atributo url é preenchido com o valor que vem do app-component.html
+
+}
