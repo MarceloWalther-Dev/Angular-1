ff01e30b7b44967e7694fbc35a9b4ce31fb691df
MarceloWalther-Dev <marcelo.walther.10@gmail.com>
2021-08-23 11:42:44 -0300

    redirecionamento apos o login correto para rota de fotos

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignInComponent } from './signin/signin/signin.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { VmessageModule } from '../shared/vmessage/vmessage/vmessage.module';
 
 
 
@@ -10,6 +11,7 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    VmessageModule
   ]
 })
 export class HomeModule { }
